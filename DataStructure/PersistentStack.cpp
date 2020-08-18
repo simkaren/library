8e925273d1c89e7cb36ce0c0387db29dd8bb44be
simkaren <qawsedrftgyhujikolmnbvcxz9@gmail.com>
2020-08-18 09:48:23 +0900

    永続stackの追加

---
+++ DataStructure/PersistentStack.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+struct PersistentStack {
+	struct node {
+		T value;
+		node* prv;
+		node(T value, node* prv) {
+			this->value = value;
+			this->prv = prv;
+		}
+	};
+private:
+	node* p;
+public:
+	PersistentStack(void) : p(nullptr) {}
+	PersistentStack(node* ptr) : p(ptr) {}
+	T top(void) {
+		assert(("top() called on an empty stack.", p != nullptr));
+		return (p->value);
+	}
+	PersistentStack push(T value) {
+		node* new_node = new node(value, p);
+		return PersistentStack(new_node);
+	}
+	PersistentStack pop(void) {
+		assert(("pop() called on an empty stack.", p != nullptr));
+		return PersistentStack(p->prv);
+	}
+};
