4340156224567dd9405936cea365fda2c9000e12
simkaren <qawsedrftgyhujikolmnbvcxz9@gmail.com>
2021-06-28 16:51:59 +0900

    updated geometry

--- Math/Geometry.cpp
+++ Math/Geometry.cpp
@@ -92,24 +92,14 @@ long double absl(const point<T>& p){
 }
 template<typename T>
 double abs(const point<T>& p){ return (double)absl(p); }
-template<typename T>
-float absf(const point<T>& p){ return (float)absl(p); }
 
 // |l - r|
 template<typename T>
 long double distl(const point<T>& l, const point<T>& r){ return absl(l - r); }
 template<typename T>
 double dist(const point<T>& l, const point<T>& r){ return (double)distl(l, r); }
-template<typename T>
-float distf(const point<T>& l, const point<T>& r){ return (float)distl(l, r); }
 
 // rotate p by theta around the origin
-point<float> rotate(const point<float>& p, float theta){
-    return point<float>{
-        cosf(theta) * p.first - sinf(theta) * p.second,
-        sinf(theta) * p.first - cosf(theta) * p.second
-    };
-}
 point<double> rotate(const point<double>& p, double theta){
     return point<double>{
         cos(theta) * p.first - sinf(theta) * p.second,
@@ -138,10 +128,6 @@ long double anglel(const point<T>& p1, const point<T>& p2, const point<T>& p3){
 template<typename T>
 double angle(const point<T>& p1, const point<T>& p2, const point<T>& p3){
     return (double)anglel(p1, p2, p3);
-}
-template<typename T>
-float anglef(const point<T>& p1, const point<T>& p2, const point<T>& p3){
-    return (float)anglel(p1, p2, p3);
 }
 
 template<typename T>
@@ -226,13 +212,18 @@ long double areal(const polygon<T>& p){
 }
 template<typename T>
 double area(const polygon<T>& p){ return (double)areal(p); }
-template<typename T>
-float areaf(const polygon<T>& p){ return (float)areaf(p); }
 
 template<typename T>
-using line = std::pair<point<T>, point<T>>;
-template<typename T>
 using segment = std::pair<point<T>, point<T>>;
+
+template<typename T>
+struct line{
+    point<T> first, second;
+    line() {}
+    line(point<T> p1, point<T> p2){
+        first = p1, second = p2;
+    }
+};
 
 // l1 // l2?
 template<typename T>
@@ -263,11 +254,6 @@ point<double> projection(const point<T>& p, const line<T>& l){
     point<long double> tmp = projectionl(p, l);
     return {(double)tmp.first, (double)tmp.second};
 }
-template<typename T>
-point<float> projectionf(const point<T>& p, const line<T>& l){
-    point<long double> tmp = projectionl(p, l);
-    return {(float)tmp.first, (float)tmp.second};
-}
 
 // reflection of p onto l
 template<typename T>
@@ -282,10 +268,113 @@ point<double> reflection(const point<T>& p, const line<T>& l){
     point<long double> tmp = reflectionl(p, l);
     return {(double)tmp.first, (double)tmp.second};
 }
+
+// counter clockwise
+// 1: p0, p1, p2 make a counterclockwise turn
+// 2: p0, p1, p2 make a clockwise turn
+// 3: p2 is on p0-p1 line and p2, p0, p1 in this order
+// 4: p2 is on p0-p1 line and p0, p1, p2 in this order
+// 5: p2 is on a segment p0-p1
 template<typename T>
-point<float> reflectionf(const point<T>& p, const line<T>& l){
-    point<long double> tmp = reflectionl(p, l);
-    return {(float)tmp.first, (float)tmp.second};
+int ccw(const point<T>& p0, const point<T>& p1, const point<T>& p2, long double eps = 1e-8L){
+    point<T> v1 = p1 - p0, v2 = p2 - p0;
+    if (cross(v1, v2) > eps) return 1;
+    if (cross(v1, v2) < -eps) return 2;
+    if (dot(v1, v2) < 0) return 3;
+    if (absl(v1) < absl(v2)) return 4;
+    return 5;
+}
+
+// intersect
+template<typename T>
+bool intersect(const segment<T>& s1, const segment<T>& s2, long double eps = 1e-8L){
+    auto f = [](int c) -> int{
+        if (c == 1 || c == 3) return 1;
+        else if (c == 2 || c == 4) return -1;
+        return 0;
+    };
+    int ccw1 = f(ccw(s1.first, s1.second, s2.first, eps));
+    int ccw2 = f(ccw(s1.first, s1.second, s2.second, eps));
+    int ccw3 = f(ccw(s2.first, s2.second, s1.first, eps));
+    int ccw4 = f(ccw(s2.first, s2.second, s1.second, eps));
+    return (ccw1 * ccw2 <= 0 && ccw3 * ccw4 <= 0);
+}
+template<typename T>
+bool intersect(const segment<T>& s, const line<T>& l, long double eps = 1e-8L){
+    point<T> ls_f = l.second - l.first;
+    return cross(ls_f, s.first - l.first) * cross(ls_f, s.second - l.first) < eps;
+}
+
+// cross point
+template<typename T>
+point<long double> crosspointl(const line<T>& l1, const line<T>& l2, long double eps = 1e-8L){
+    long double crs1 = cross(l1.second - l1.first, l2.second - l2.first);
+    long double crs2 = cross(l1.second - l1.first, l1.second - l2.first);
+    if (std::abs(crs1) < eps && std::abs(crs2) < eps)
+        return {(long double)l2.first.first, (long double)l2.first.second};
+    long double x = l2.first.first + (l2.second.first - l2.first.first) * crs2 / crs1;
+    long double y = l2.first.second + (l2.second.second - l2.first.second) * crs2 / crs1;
+    return {x, y};
+}
+template<typename T>
+point<double> crosspoint(const line<T>& l1, const line<T>& l2, long double eps = 1e-8L){
+    point<long double> cp = crosspointl(l1, l2, eps);
+    return {(double)cp.first, (double)cp.second};
+}
+
+// distance
+template<typename T>
+long double distl(const point<T>& p, const line<T>& l){
+    point<long double> prj = projectionl(p, l);
+    prj.first -= p.first;
+    prj.second -= p.second;
+    return absl(prj);
+}
+template<typename T>
+double dist(const point<T>& p, const line<T>& l){
+    return (double)distl(p, l);
+}
+template<typename T>
+long double distl(const point<T>& p, const segment<T>& s, long double eps = 1e-8L){
+    line<T> l(s.first, s.second);
+    point<long double> prj = projectionl(p, l);
+    point<long double> s1{(long double)s.first.first, (long double)s.first.second};
+    point<long double> s2{(long double)s.second.first, (long double)s.second.second};
+    int c = ccw(s1, s2, prj, eps);
+    if (c == 5){
+        prj.first -= p.first;
+        prj.second -= p.second;
+        return absl(prj);
+    }
+    point<long double> p_ld{(long double)p.first, (long double)p.second};
+    point<long double> tmp1 = s1 - p_ld;
+    point<long double> tmp2 = s2 - p_ld;
+    return std::min(absl(tmp1), absl(tmp2));
+}
+template<typename T>
+double dist(const point<T>& p, const segment<T>& s, long double eps = 1e-8L){
+    return (double)distl(p, s, eps);
+}
+template<typename T>
+long double distl(const segment<T>& s1, const segment<T>& s2, long double eps = 1e-8L){
+    if (intersect(s1, s2, eps)) return 0;
+    return std::min({
+        distl(s1.first, s2, eps), distl(s1.second, s2, eps),
+        distl(s2.first, s1, eps), distl(s2.second, s1, eps)
+    });
+}
+template<typename T>
+double dist(const segment<T>& s1, const segment<T>& s2, long double eps = 1e-8L){
+    return (double)distl(s1, s2, eps);
+}
+template<typename T>
+long double distl(const segment<T>& s, const line<T>& l, long double eps = 1e-8L){
+    if (intersect(s, l, eps)) return 0;
+    return std::min(distl(s.first, l), distl(s.second, l));
+}
+template<typename T>
+double dist(const segment<T>& s, const line<T>& l, long double eps = 1e-8L){
+    return (double)distl(s, l, eps);
 }
 
 } // namespace geometry
